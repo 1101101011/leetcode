f31a351d16017f25915a9820c4d1edff150cad6b
1101101011 <ilardecharles@gmail.com>
2024-09-15 23:28:43 +0800

    some stuffs

--- dsa/lists.c
+++ dsa/lists.c
@@ -8,20 +8,21 @@ int main() {
   printw("Hello, NixOS with ncurses!"); // Print message
   refresh();                            // Refresh the screen to show the output
   getch();                              // Wait for user input
+  clear();                              // Clear the screen
   cbreak();                             // Disable line buffering
-  keypad(stdscr, TRUE); // Enable reading of function keys (e.g., F1, arrow keys)
-  noecho();     // Don't echo() while we do getch
-  curs_set(0);  // Hide the cursor
+  keypad(stdscr, TRUE);                 // Enable reading of function keys (e.g., F1, arrow keys)
+  noecho();                             // Don't echo() while we do getch
+  curs_set(0);                          // Hide the cursor
 
   // Print message
   mvprintw(10, 20, "Hello, ncurses!"); // Print at row 10, column 20
   refresh();                           // Print on the screen
 
   // Wait for user input
-  getch(); // Wait for user input (it will block until a key is pressed)
+  getch();                             // Wait for user input (it will block until a key is pressed)
 
   // Clean up and end ncurses mode
-  endwin(); // End curses mode
+  endwin();                            // End curses mode
 
   return 0;
 }
---
+++ leetcode-problems/two-sum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int outer(int *nums, int numsSize, int target, int *returnSize) {
+  if(numsSize < 4) {
+    if(nums[0] + nums[1] == target) {
+      printf("[%d, %d]\n", 0, 1);
+      *returnSize 2;
+    }
+  }
+}
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) { 
+  if(numsSize < 4) {
+    outer(nums, numsSize, target, returnSize);
+    twoSum(nums, numsSize, target, returnSize);
+  }
+  *returnSize = 0;
+}
+
+int main() {
+  int nums[] = {2, 7, 11, 15};
+  int target = 9;
+  int i, j;
+  // for(i = 0; i < 4; i++) {
+  //     for(j = i + 1; j < 4; j++) {
+  //         if(nums[i] + nums[j] == target) {
+  //             printf("[%d, %d]\n", i, j);
+  //             return 0;
+  //         }
+  //     }
+  // }
+  twoSum(nums, 4, target, 0); 
+  return 0;
+}
